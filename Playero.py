9b5e0359883c017228ce18abdfd942c465ea6773
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-05-20 12:28:21 -0400

    implementation of attributes and methods inferered from Classes

--- Playero.py
+++ Playero.py
@@ -1,9 +1,10 @@
 from astroid import MANAGER
 from astroid.builder import AstroidBuilder
 from astroid import scoped_nodes
-from funcs import getRecordsInfo, findPaths
+from funcs import getRecordsInfo, findPaths, getClassInfo
 
 built = ["Record","RawRecord"]
+
 
 def hashlib_transform(module):
     modname = module.name
@@ -17,32 +18,49 @@ def hashlib_transform(module):
                 module.locals[fields] = buildIterator(modname, fields, detrecords[detailname])
             else:
                 module.locals[fields] = records[modname][fields]
-        module.locals["bring"] = buildBring(modname, records[modname])
+        attributes, methods = getClassInfo(modname)
+        instanceFields = list(records[modname]) + list(attributes)
+        module.locals["bring"] = buildBring(modname, instanceFields)
+
+        module.locals.update([(attr, {0:None}) for attr in attributes])
+        module.locals.update([("_"+meth, buildMethod(modname, meth)) for meth in methods])
+
 
 def buildIterator(name, detailfield, fields):
-    built.append("%s_%s" % (name, detailfield))
+    itername = "%s_%s" % (name, detailfield)
+    built.append(itername)
     fieldTxt = ["%s%s%s" % ("        self.", x," = None") for x in fields]
     fake = AstroidBuilder(MANAGER).string_build('''
-class %s_%s(object):
+class %s(object):
     def __iter__(self):
         return self
 
+    def count(): pass
+
     def __init__(self, *args):
         self.__fail__ = None
 %s
-''' % (name, detailfield, "\n".join(fieldTxt)))
-    return fake.locals["%s_%s" % (name, detailfield)]
+''' % (itername, "\n".join(fieldTxt)))
+    return fake.locals[itername]
+
 
 def buildBring(name, fields):
-    built.append("%s_%s" % (name, "bring"))
+    bringname = "%s_%s" % (name, "bring")
+    built.append(bringname)
     fieldTxt = ["%s%s%s" % ("        self.", x," = None") for x in fields]
     fake = AstroidBuilder(MANAGER).string_build('''
-class %s_bring(object):
+class %s(object):
     def __init__(self, *args):
         self.__fail__ = None
 %s
-''' % (name, "\n".join(fieldTxt)))
-    return fake.locals[name+"_bring"]
+''' % (bringname, "\n".join(fieldTxt)))
+    return fake.locals[bringname]
+
+
+def buildMethod(name, method):
+    methodname = "%s_%s" % (name, method)
+    fake = AstroidBuilder(MANAGER).string_build("def %s(): pass" % methodname)
+    return  {0:fake[methodname]}
 
 def register(linter):
     pass
