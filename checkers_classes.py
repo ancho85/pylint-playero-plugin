23f0bf0d8229b822dc3a0bad6ca9488170aa8d48
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2015-02-25 15:53:17 -0300

    [UPD] getGetattrValue. getHeirValue added to locate values created via inheritance

--- checkers_classes.py
+++ checkers_classes.py
@@ -342,17 +342,44 @@ class QueryChecker(BaseChecker):
             self.logError("getTupleValuesError", nodeValue, e)
         return '("""%s""")' % '""","""'.join(tvalues)
 
+    def getHeirValue(self, node):
+        """locate attribute in inheritance by visiting all assignments"""
+        heirVal = ""
+        prevSi = node.previous_sibling()
+        while prevSi is not None:
+            if isinstance(prevSi, Assign):
+                if isinstance(prevSi.targets[0], AssName) and prevSi.targets[0].name == node.expr.name:
+                    if isinstance(prevSi.value, CallFunc) and isinstance(prevSi.value.func, Getattr):
+                        if prevSi.value.func.expr.name == "self":
+                            if prevSi.value.func.attrname not in node.scope().parent.locals: #the function called is not present
+                                heirClass = node.scope().parent.bases[0].infered()[0]
+                                heirCall = heirClass.locals[prevSi.value.func.attrname][0]
+
+                                #Now I'm building self.queryTxt by visiting all Assigns and AugAssigns
+                                for ass in heirCall.body:
+                                    if isinstance(ass, Assign):
+                                        self.visit_assign(ass)
+                                    elif isinstance(ass, AugAssign):
+                                        self.visit_augassign(ass)
+                                if node.expr.name in self.queryTxt:
+                                    heirVal = self.queryTxt[node.expr.name]
+                                    break #value found, breaks the while
+            prevSi = prevSi.previous_sibling()
+        return heirVal
+
     def getGetattrValue(self, nodeValue):
         res = ""
         try:
-            gvalue = nodeValue.attrname
             inferedValue = nodeValue.expr.infered()[0]
             if isinstance(inferedValue, Instance) :
                 if isinstance(inferedValue.infered()[0], Class):
-                    if inferedValue.pytype() == "Query.Query" and nodeValue.expr.name in self.queryTxt:
-                        res = self.queryTxt[nodeValue.expr.name]
-                    else:
-                        res = self.getClassAttr(inferedValue.infered()[0], gvalue)
+                    if inferedValue.pytype() == "Query.Query":
+                        if nodeValue.expr.name in self.queryTxt:
+                            res = self.queryTxt[nodeValue.expr.name]
+                        else:
+                            res = self.getHeirValue(nodeValue)
+                    if not res:
+                        res = self.getClassAttr(inferedValue.infered()[0], nodeValue.attrname)
             elif isinstance(nodeValue.expr, Subscript):
                 res = self.getSubscriptValue(nodeValue.expr)
         except InferenceError:
