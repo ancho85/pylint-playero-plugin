add32f9594aefbc9b2fd0bea8f0cb7cad73942eb
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2015-03-11 14:08:52 -0300

    fulltest includes to syspath corepy/embedded. adding more transforms to tests#22

--- tests/fulltest.py
+++ tests/fulltest.py
@@ -6,15 +6,16 @@ from pylint.testutils import make_tests, LintTestUsingFile, cb_test_gen, linter
 import ConfigParser
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+PLUGINPATH = os.path.join(HERE, "..")
 
-linter.prepare_import_path(os.path.join(HERE, ".."))
+linter.prepare_import_path(PLUGINPATH)
 linter.load_plugin_modules(['Playero'])
 linter.global_set_option('required-attributes', ())  # remove required __revision__
 linter.load_file_configuration(os.path.join(HERE, "..", "config", ".pylintrc"))
 
 convs = ['C0111', 'C0103', 'C0301', 'C0303', 'C0304', 'C0321']
 warns = ['W0141', 'W0142', 'W0212', 'W0312', 'W0401', 'W0403', 'W0511', 'W0512', 'W0614', 'W0622']
-refac = ['R0903', 'R0913']
+refac = ['R0903', 'R0904', 'R0913']
 for disabled in convs + warns + refac:
     linter.disable(disabled)
 
@@ -25,6 +26,7 @@ sys.path.append(os.path.join(PLAYEROPATH, "core"))
 for scriptdir in ["base", "standard", "StdPy"]:
     for pydir in ['records', 'windows', 'reports', 'routines', 'documents','tools']:
         sys.path.append(os.path.join(PLAYEROPATH, scriptdir, pydir))
+sys.path.append(os.path.join(PLUGINPATH, "corepy", "embedded"))
 
 def tests():
     callbacks = [cb_test_gen(LintTestUsingFile)]
--- tests/input/func_noerror_transforms.py
+++ tests/input/func_noerror_transforms.py
@@ -1,8 +1,11 @@
 """
-Checks that Pylint does not complain about SuperClass transform
+Checks that Pylint does not complain about reserved words
 """
 from OpenOrange import *
 
 ParentMyRecord = SuperClass("MyRecord", "MyInheritance", __file__)
 class MyRecord(ParentMyRecord):
-    pass
+
+    rec = NewRecord("MyRec")
+    win = NewWindow("MyWin")
+    rou = NewReport("MyRep")
