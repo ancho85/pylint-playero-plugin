9b2a336bc9e39927076f7fdee680bff54907520f
Carlos Gomez <ancho.corp@gmail.com>
2015-03-11 00:05:00 -0300

    [FIX] removing coveralls.io support from master, a new branch was created for this feature

--- tests/input/func_noerror_superclass.py
+++
@@ -1,8 +0,0 @@
-"""
-Checks that Pylint does not complain about SuperClasses
-"""
-from OpenOrange import *
-
-MyInvoice = SuperClass("Invoice", "SalesTransaction", __file__)
-class Invoice(MyInvoice):
-    pass
