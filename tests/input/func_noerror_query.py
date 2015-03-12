9b5ac83751fd6e5a4c0c9d3e0148055e18d525d8
Carlos Gomez <ancho.corp@gmail.com>
2015-03-11 22:55:35 -0300

    increasing coverage of query tests

--- tests/input/func_noerror_query.py
+++ tests/input/func_noerror_query.py
@@ -2,6 +2,9 @@
 
 from OpenOrange import *
 from Routine import Routine
+from Document import Document
+from Label import Label
+from SQLTools import codeOrder, monthCode
 
 class InvoiceAlter(Routine):
 
@@ -41,4 +44,77 @@ class InvoiceAlter(Routine):
 
         query.sql += ") AS final \n"
         query.sql += "ORDER BY RoomType"
+        query.open()
+
+class AlotmentDoc(Document):
+
+    classattr = "classattr"
+
+    def getRecorda(self):
+        class newObj(object):
+            Status = 1
+            RootLabel = "100"
+            SerNr = "SerNr"
+            def name(self):
+                print str(self)
+                return "Alotment"
+        return newObj()
+
+    def getExtra(self, val1, val2="2", val3="3", val4=4):
+        specs = self.getRecorda()
+        sql = "WHERE?AND [al].{%s} IN ('%s')\n" % ("SerNr", "','".join([val1, val2]))
+        sql += "WHERE?AND [al].{SerNr} = i|%i|\n" % specs.Status
+        sql += "WHERE?AND SerNr = "
+        if specs.Status == 1:
+            sql += "%s" % val4
+        if 1 in [0, 0]:
+            pass
+        else:
+            sql += ""
+        return sql
+
+    def getExtra2(self, test):
+        specs = self.getRecorda()
+        mydict = {1:1, 2:2}
+        mylist = [1]
+        listcomp = "listcomp"
+        if test > 0:
+            return specs.Status
+        x =  "'%s', " % date("")
+        x += "'%s', " % time("")
+        x += "'%i', " % len(specs.RootLabel)
+        x += "'%s', " % filter(None, ["1","2"])
+        x += "'%s', " % mydict.keys()
+        x += "'%s', " % "replice".replace("i", "a")[0]
+        x += "'%s', " % "split,splet".split(",")[0]
+        x += "'%s', " % mylist.extend([2])
+        x += "'%s', " % self.classattr
+        x += "'%s', " % self.classattr
+        x += "'%s', " % mydict
+        x += "'%s', " % self.__field_decimals__ #Parent dictionary attribute
+        x += "'%s', " % mylist + mylist
+        x += "'%s', " % [a for a in ("1","2")]
+        x += "'%s', " % "".join([str(b) for b in listcomp])
+        x += "'%s', " % "".join([c.strip() for c in specs.name()])
+        x += "'%s', " % "".join(["%sbinop" % d for d in specs.name()])
+        return x
+
+    def getExtra3(self):
+        specs = self.getRecorda()
+        subquery = Query()
+        subquery.sql = "SerNr"
+        return "ORDER BY %s, %s" % (specs.SerNr, subquery.sql)
+
+    def run(self):
+        specs = self.getRecorda()
+        leaves = Label.getTreeLeaves(specs.RootLabel)
+        query = Query()
+        query.sql = "SELECT SerNr, %s,\n" % codeOrder("SerNr", leaves)
+        query.sql += monthCode("[al].TransDate")
+        query.sql += "\n, %s" % self.getExtra2(test=1)
+        query.sql += "\n, %s" % self.getExtra2(test=0)
+        query.sql += "\nFROM %s al\n" % specs.name()
+        query.sql += self.getExtra("1", "2", val3="33")
+        method = getattr(self, "getExtra3lala"[:-4])
+        query.sql += method()
         query.open()
