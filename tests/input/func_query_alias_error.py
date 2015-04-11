928661096221529b1b3413d47cc9b2dda303cf27
Carlos Gomez <ancho.corp@gmail.com>
2015-04-10 21:20:26 -0400

    [FIX] query syntax check file separation

--- tests/input/func_query_alias_error.py
+++ tests/input/func_query_alias_error.py
@@ -3,5 +3,5 @@
 from OpenOrange import *
 
 errQuery = Query()
-errQuery.sql = "SELECT SerNr, FROM Alotment"
+errQuery.sql = "SELECT SerNr FROM Alotment alias1 INNER JOIN Deposit alias1"
 errQuery.open()
