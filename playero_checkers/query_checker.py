8cfcb0c8dd11cd58aabf069bd834b925709dc468
Carlos Gomez <ancho.corp@gmail.com>
2015-05-09 22:41:21 -0400

    [FIX] getAssNameValue and searchBody now returns the node where the value was found and getNameValue send that node to concatOrReplace

--- playero_checkers/query_checker.py
+++ playero_checkers/query_checker.py
@@ -93,8 +93,6 @@ class QueryChecker(BaseChecker):
                     if res == newVal: continue
                     elif previousValue and atr == "orelse": continue
                     res = self.concatOrReplace(elm, nodeName, res, newVal)
-                    if res and atr == "body":
-                        break #multiple concatenations with the same previously calculated values
             elif isinstance(node, For):
                 if isinstance(node.target, AssName) and nodeName == node.target.name:
                     res = newVal
@@ -115,17 +113,17 @@ class QueryChecker(BaseChecker):
     def getAssNameValue(self, nodeValue, nodeName="", tolineno=999999):
         if not tolineno: tolineno = 999999
         anvalue = ""
-        anfound = False
+        anfound = None
 
         def searchBody(attrs):
-            bvalue, bfound = None, False
+            bvalue, bfound = None, None
             getBVal = lambda x: "" if x is None else x
             for elm, atr in [(elm, atr) for atr in attrs for elm in getattr(nodeValue, atr) if elm.lineno < tolineno]:
                 if not (bvalue and atr == "orelse"): #no need to search in 'orelse' if the 'body' returns a value
                     (assValue, afound) = self.getAssNameValue(elm, nodeName=nodeName, tolineno=tolineno)
                     if afound and assValue != bvalue:
                         bvalue = self.concatOrReplace(elm, nodeName, getBVal(bvalue), assValue)
-                        bfound = afound
+                        bfound = elm
             return (getBVal(bvalue), bfound)
 
         try:
@@ -134,19 +132,19 @@ class QueryChecker(BaseChecker):
                 if isinstance(nodeValue.targets[0], AssName):
                     if nodeName and nodeName == nodeValue.targets[0].name:
                         anvalue = self.getAssignedTxt(nodeValue.value)
-                        anfound = True
+                        anfound = nodeValue
             elif isinstance(nodeValue, AugAssign):
                 if isinstance(nodeValue.target, AssName):
                     if nodeName and nodeName == nodeValue.target.name:
                         anvalue = self.getAssignedTxt(nodeValue.value)
-                        anfound = True
+                        anfound = nodeValue
             elif isinstance(nodeValue, For):
                 if isinstance(nodeValue.target, AssName):
                     if nodeName and nodeName == nodeValue.target.name:
                         anvalue = self.getAssignedTxt(nodeValue.iter)
                         if not anvalue.startswith("["): anvalue = "['%s']" % anvalue
                         if anvalue == "[]": anvalue = "['666']"
-                        anfound = True
+                        anfound = nodeValue
                         try:
                             anvalue = ast.literal_eval(anvalue)[0]
                         except Exception, e:
@@ -165,7 +163,7 @@ class QueryChecker(BaseChecker):
                     if isinstance(nvf, Getattr) and isinstance(nvf.expr, Name):
                         if nodeName == nvf.expr.name:
                             anvalue = self.getCallFuncValue(nodeValue.value)
-                            anfound = True
+                            anfound = nodeValue
         except Exception, e:
             self.logError("getAssNameValueError", nodeValue, e)
         return (anvalue, anfound)
@@ -196,7 +194,7 @@ class QueryChecker(BaseChecker):
                 if elm.lineno >= nodeValue.lineno: break #finding values if element's line is previous to node's line
                 (assValue, assFound) = self.getAssNameValue(elm, nodeName=nodeValue.name, tolineno=nodeValue.parent.lineno)
                 if assFound:
-                    nvalue = self.concatOrReplace(elm, nodeValue.name, nvalue, assValue)
+                    nvalue = self.concatOrReplace(assFound, nodeValue.name, nvalue, assValue)
                     tryinference = False
             if not nvalue and tryinference:
                 try:
